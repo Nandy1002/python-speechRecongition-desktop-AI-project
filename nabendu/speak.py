9d836a0f4c3e9f2612e2610b6acf3594170ff46f
Nandy1002 <nabendudas2001@gmail.com>
2023-03-18 01:49:59 +0530

    added speak and speechToText functionality

---
+++ nabendu/speak.py
@@ -0,0 +1,14 @@
+import pyttsx3
+# import speech_recognition
+# import tkinter as Tk
+
+#configuring speak engine with pyttsx3
+engine = pyttsx3.init() #initialization
+engine.setProperty('voice', 'english_rp+f5') #setting the female voice
+engine.setProperty('rate',130) #speaking speed = 130 (default 200)
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello I am IRIS")
+
---
+++ nabendu/speakToText.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr  
+import os
+
+# get audio from the microphone                                                                       
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        os.system('clear')
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"You said: {query}\n")  #User query will be printed.
+
+    except Exception as e:
+    # print(e)    
+        print("Say that again please...")   #Say that again will be printed in case of improper voice 
+        return "None" #None string will be returned
+    return query
+
+takeCommand()
