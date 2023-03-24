d1ac7cc523c0f0a0cee0e5058b547ee39a27015d
Nandy1002 <nabendudas2001@gmail.com>
2023-03-25 00:24:26 +0530

    modified

--- nabendu/speak.py
+++ nabendu/speak.py
@@ -4,7 +4,7 @@ import pyttsx3
 
 #configuring speak engine with pyttsx3
 engine = pyttsx3.init() #initialization
-engine.setProperty('voice', 'tamil_rp+f5') #setting the female voice
+engine.setProperty('voice', 'english_rp+f5') #setting the female voice
 engine.setProperty('rate',130) #speaking speed = 130 (default 200)
 def speak(text):
     engine.say(text)
