81878a09d16af20ebf582db469e658b5ec3cbf45
Nandy1002 <nabendudas2001@gmail.com>
2023-03-26 01:25:24 +0530

    added another speak function without pyttsx3

---
+++ nabendu/speak2.py
@@ -0,0 +1,9 @@
+import os
+
+# generate voice using mbrolla voice without pyttsx3
+def speak(text):
+    print(f"IRIS Said : {text}")
+    text = "'"+text+"'"
+    os.system(f"espeak-ng -v mb-us1 -s 140 {text}")
+
+# speak("This is a sample text using embrolla voice pack")
